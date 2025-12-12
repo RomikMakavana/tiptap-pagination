8a591c730258841c34fc34dae739a0e03b693dbf
Romik <40670677+RomikMakavana@users.noreply.github.com>
2025-12-12 14:56:33 +0530

    Page Break & Freeze Issue

--- src/ui/open-for-work.tsx
+++ src/ui/open-for-work.tsx
@@ -3,7 +3,7 @@ import { Button } from "./button";
 export const OpenForWork = () => {
   return (
     <div
-      className="fixed bottom-0 border rounded-lg shadow-sm shadow-amber-400 p-2 bg-muted/90 backdrop-blur-md z-50 mb-4"
+      className="fixed bottom-0 border rounded-lg shadow-sm shadow-blue-100 p-2 bg-muted/90 backdrop-blur-md z-50 mb-4"
       style={{
         right: "50vw",
         marginLeft: "auto",
@@ -11,20 +11,23 @@ export const OpenForWork = () => {
         transform: "translateX(50%)",
       }}
     >
-      <div className="flex flex-row items-center justify-between gap-2" style={{width: "max-content"}}>
-        <div>
-          <h1 className="text-sm">
-            {" "}
+        {/* <div className="w-full">
+          <h1 className="text-sm mb-1">
             I'm <span className="font-bold">Open for Work</span>
           </h1>
-          <p className="text-xs">Let's talk about your project</p>
-
-        </div>
+        </div> */}
+      <div className="flex flex-row items-center justify-between gap-2" style={{width: "max-content"}}>
         <a href="https://forms.gle/KGVAhKSUnjFnGaAw8" target="_blank" rel="noopener noreferrer">
           <Button variant="outline" className="!bg-amber-400 text-black hover:text-amber-50 !transition-all duration-150">
-            Contact Me
+            Contact
           </Button>
         </a>
+        <a href="https://discord.gg/6B2xYsHgPT" target="_blank" rel="noopener noreferrer">
+          <Button variant="ghost" className="!text-white !hover:bg-white !hover:text-black" style={{background: "linear-gradient(135deg, #5865F2 0%, #4752C4 100%)"}}>
+            Join Discord
+          </Button>
+        </a>
+        
       </div>
     </div>
   );
